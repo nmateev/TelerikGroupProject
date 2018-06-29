6cc17b80bf5cdea2c150c4ac5bf02dd6a50d508e
Nikolay Mateev <nmateev@abv.bg>
2018-06-29 16:15:14 +0300

    Adding all files

---
+++ Project/src/onlineshop/users/User.java
@@ -0,0 +1,7 @@
+package onlineshop.orders;
+
+public class User {
+    private String userName;
+    private String phone;
+    private String address;
+}
---
+++ Project/src/onlineshop/users/VipUser.java
@@ -0,0 +1,4 @@
+package onlineshop.orders;
+
+public class UserType {
+}
