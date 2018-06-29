b7aae5bb4bfca2287543d9d78d8ff55a45b036cf
Nikolay Mateev <nmateev@abv.bg>
2018-06-29 17:12:02 +0300

    Add all

---
+++ Project/src/onlineshop/shipping/ShippingType.java
@@ -0,0 +1,5 @@
+package shipping;
+
+public enum ShippingType {
+    STANDART , EXPRESS , FREE
+}
---
+++ Project/src/onlineshop/shipping/StandartShipping.java
@@ -0,0 +1,22 @@
+package shipping;
+
+public class StandartShipping extends Shipping {
+    final static String DELIVERY_TIME="0-1 Week";
+    @Override
+    public void setType(ShippingType type) {
+        type= ShippingType.STANDART;
+        super.setType(type);
+    }
+
+    @Override
+    public void setDeliveryTime(String deliveryTime) {
+        deliveryTime=DELIVERY_TIME;
+        super.setDeliveryTime(deliveryTime);
+    }
+
+
+    @Override
+    public void setPrice(double price) {
+        super.price=price;
+    }
+}
