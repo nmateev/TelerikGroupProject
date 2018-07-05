2156c16704bff1f8fab65435651e914aa3737114
Nikolay Mateev <nmateev@abv.bg>
2018-07-05 22:43:10 +0300

    Cleaned directory

--- Project/src/onlineshop/product/LoadableDatabase.java
+++
@@ -1,7 +0,0 @@
-package onlineshop.product;
-
-//Interface  which when implemented by a database will add the option for the database to be loaded
-public interface LoadableDatabase {
-
-    void loadProductDatabase();
-}
